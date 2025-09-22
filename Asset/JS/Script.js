579d028885440c757c9697a4c94d31d12ed9320c
Rfrob <blaxblou@gmail.com>
2025-09-22 12:52:52 +0200

    first commit

---
+++ Asset/JS/Script.js
@@ -0,0 +1,13 @@
+//console.log("Hello World");
+
+//var a = 5; // a ne plus utiliser
+/*const a = 5;
+let b = 8;
+alert(a + b);*/
+
+let age = 43; //number
+let nom = "Robi";//chaine de caractère
+let majeur = true; //boolean
+const pi = 3.14; //constante
+
+
